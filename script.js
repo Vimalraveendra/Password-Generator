85c337ca3b8f7e397570fd8fbfd41932d238e530
vimal Raveendran <vimalraveendra@gmail.com>
2020-11-04 13:00:19 +0100

    updated html, script & styles sheet

---
+++ script.js
@@ -0,0 +1,15 @@
+const pass = document.getElementById(' pass')
+const copy = document.getElementById('copy')
+const length = document.getElementById('length')
+const upper = document.getElementById('upper')
+const lower = document.getElementById('lower')
+const number = document.getElementById(' numbers')
+const symbol = document.getElementById('symbols')
+const generator = document.getElementById('btn-generator')
+
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTVUWXYZ';
+const lowerLetters ='abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols ='!@#$%^&*()_+';
+
